70f1c0a0db3fb6755a52e2469c8fdff38b2145f0
florentina93 <and.dragu@gmail.com>
2016-07-05 10:02:00 +0300

    Project UI5

---
+++ MyFioriUI5/BuildingSAPFiori-likeUIsWithSAPUI5_Projects/myFiori7/view/Detail.controller.js
@@ -0,0 +1,13 @@
+jQuery.sap.require("sap.ui.demo.myFiori.util.Formatter");
+
+sap.ui.controller("sap.ui.demo.myFiori.view.Detail", {
+
+	handleNavButtonPress : function (evt) {
+		this.nav.back("Master");
+	},
+
+	onBeforeRendering:function(){
+		this.byId("SupplierForm").bindElement("BusinessPartner");
+	}
+	
+});
---
+++ MyFioriUI5/WebContent/view/LineItem.controller.js
@@ -0,0 +1,5 @@
+sap.ui.controller("sap.ui.demo.myFiori.view.LineItem", {  
+	handleNavBack : function (evt) {    
+		this.nav.back("Detail");  
+		} 
+}); 
