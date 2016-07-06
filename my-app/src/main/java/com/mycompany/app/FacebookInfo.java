ecfc8521ea1df71edd00949a42666347936d2576
Patricia008 <petrut.patricia@gmail.com>
2016-07-06 14:53:23 +0300

    postInfoOnPage

--- my-app/src/main/java/com/mycompany/app/FacebookInfo.java
+++ my-app/src/main/java/com/mycompany/app/FacebookInfo.java
@@ -1,5 +1,13 @@
 package com.mycompany.app;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
@@ -10,9 +18,10 @@ import com.restfb.types.FacebookType;
 import com.restfb.types.Page;
 import com.restfb.types.User;
 
-public class FacebookInfo {
+public class FacebookInfo extends HttpServlet{
+	
 	 /* Variables */
-    private final String pageAccessToken = "EAACEdEose0cBAOVXirgYiHkz9Snf78CDf4e2W9DDrZA4iUqeNL5onV6csxRJcbcRV2kU99KMrXcrgVuGauoNZAKdCcFopUl1bWkdyFjBR92NmTP8wLtdvCF9VbutA6EB4xZA4Y1sNpzNy1RsHge66reV6dYBBsUDTZC7ZBUSEhQZDZD";
+    private final String pageAccessToken = "EAACEdEose0cBAPZAJGQabh30sxqjZBgL2MyCugeG3y2xBH1svKahl5VXI0Kq40Pbb6ytZAJMZC5MFEZCkIf4Nzjt4jZClnWbZBWVlzTjZAJ5drcf9fiFZAChOEZCzdx1fCG6YhUbrRS2pQagTwvTbI61la9EBkZCF4ltZCZAK4bGYdtyMsgZDZD";
     private final String pageID = "10205237715641106";
     private FacebookClient fbClient;
     private User myuser = null;    //Store references to your user and page
@@ -41,43 +50,50 @@ public class FacebookInfo {
         counter++;
     }
     
-    public JsonObject getFriend(String name){
-    	FacebookClient facebookClient = new DefaultFacebookClient(pageAccessToken);
-    	Connection<User> myFriends = facebookClient.fetchConnection("me/friends", User.class);
+    public JsonObject getMyInfo(FacebookClient fbClient){
+    	JsonObject info = fbClient.fetchObject("me", JsonObject.class, Parameter.with("fields",  "name,"
+    			+ "									 email, location, relationship_status, religion, political, work, gender"));
+    	return info;
+    }
+    
+    public JsonObject getFriend(FacebookClient fbClient,String name){
+    	Connection<User> myFriends = fbClient.fetchConnection("me/friends", User.class);
 
-    	System.out.println("Count of my friends- " + myFriends.getTotalCount()); 
+    	System.out.println("Count of my friends " + myFriends.getTotalCount()); 
     	
     	System.out.println(myFriends.toString());
     	
-    	JsonObject userData = facebookClient.fetchObject("me",
+    	JsonObject userData = fbClient.fetchObject("me/friends",
                 JsonObject.class, Parameter.with("fields", "name, first_name, hometown"));
     	
     	return userData;
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	resp.getWriter().println("Testing Succes!");
+    	
     }
     
     public static void main(String[] args)
     {
     	FacebookInfo p = new FacebookInfo();
     	//p.makeTestPost();
-		FacebookClient facebookClient= new DefaultFacebookClient(p.getAccessToken());
+		FacebookClient facebookClient= new DefaultFacebookClient(p.getAccessToken());           
         
-        User user = facebookClient.fetchObject("me", User.class);
-
-        System.out.println("User="+ user);
-        System.out.println("UserName= "+ user.getFirstName());
-        System.out.println("Birthday= "+ user.getBirthday());
-        System.out.println("Email= "+ user.getEmail());
-        
-        JsonObject userData = facebookClient.fetchObject("me",
-                JsonObject.class, Parameter.with("fields", "name, first_name, hometown"));
- 
-        System.out.println("userData=" + userData);
- 
-        System.out.println("FirstName=" + userData.getString("first_name"));
-        System.out.println("Name= " + userData.getString("name"));
-        System.out.println("hometown= " + userData.getString("hometown"));
-        
-        JsonObject friends = p.getFriend("");
+//        JsonObject userData = facebookClient.fetchObject("me",
+//                JsonObject.class, Parameter.with("fields", "name, first_name, hometown"));
+// 
+//        System.out.println("userData=" + userData);
+// 
+//        System.out.println("FirstName=" + userData.getString("first_name"));
+//        System.out.println("Name= " + userData.getString("name"));
+//        System.out.println("hometown= " + userData.getString("hometown"));
+//        
+//        JsonObject friends = p.getFriend(facebookClient,"");
+		
+		JsonObject info = p.getMyInfo(facebookClient);
+		System.out.println(info.toString());
     }
 
 }
