2e00ffa1415b919c599405141fc9ee84eb353320
florentina93 <and.dragu@gmail.com>
2016-07-11 15:52:23 +0300

    added Web Content

--- SearchUI5/WebContent/view/Master.controller.js
+++ SearchUI5/WebContent/view/Master.controller.js
@@ -3,24 +3,29 @@ sap.ui.controller("sap.ui.demo.myFiori.view.Master", {
 	handleListItemPress : function (evt) {
 		var context = evt.getSource().getBindingContext();
 		this.nav.to("Detail", context);
-	}
+	},
 	
-//	handleSearch : function (evt) {
-//		var filters = [];
-//		var query = evt.getParameter("query");
-//		if(query && query.length > 0) {
-//			var filter = new sap.ui.model.Filter("FriendId",
-//					sap.ui.model.FilterOperator.Contains, query);
-//			
-//			filers.push(filter);
-//		}
-//		
-//		var list = this.getView().byId("list");
-//		var binding = list.getBinding("items");
-//		binding.filter(filters);
-//	}
-//	handleListSelect : function(evt) {
-//		var context = evt.getParameter("listItem").getBindingContext();
-//		this.nav.to("Detail",context);
-//	}
+		handleSearch : function (evt) {      
+				// create model filter   
+			var filters = [];   
+			var query = evt.getParameter("query"); 
+			
+			if (query && query.length > 0) {    
+				var filter1 = new sap.ui.model.Filter("FriendName", sap.ui.model.FilterOperator.Contains, query);
+				var filter2 = new sap.ui.model.Filter("FriendId", sap.ui.model.FilterOperator.Contains, query);
+				var filter3 = new sap.ui.model.Filter("FriendWorkPlace", sap.ui.model.FilterOperator.Contains, query);
+				var allFilters = new sap.ui.model.Filter([filter1, filter2,filter3],false);
+				filters.push(allFilters);   
+				}     
+			
+				// update list binding   
+			var list = this.getView().byId("list");   
+			var binding = list.getBinding("items");
+			binding.filter(filters);  
+		}
+	
+	//handleListSelect : function (evt) {   
+		//var context = evt.getParameter("listItem").getBindingContext();   
+		//this.nav.to("Detail", context);  
+	//}
 });
